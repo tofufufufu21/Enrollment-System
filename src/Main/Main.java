e1b5f3e1fde933ae7999f9aac09d1f4796ec74be
tokwa <jhonrickdc21@gmail.com>
2024-11-12 12:48:07 +0800

    update

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -2,7 +2,7 @@ package Main;
 
 import User_Types.UserType;
 
-
+// eto yung starting ng program natin
 public class Main {
     public static void main(String[] args) {
         UserType.user_type_menu();
